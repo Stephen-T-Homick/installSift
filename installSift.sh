75b811fa739bb5f6d01a740f07dcaa813f7caa75
Steve Homick <shomick87@gmail.com>
2019-04-06 09:23:29 -0400

    Next

--- installSift.sh
+++ installSift.sh
@@ -4,7 +4,7 @@ if [ $(id -u) -ne '0' ] ; then
     echo "Please run as root / sudo. Exiting"
     exit 1
 fi
-command -v dpkg || echo "Not an Ubuntu distro. Exiting" ; exit 1
+command -v dpkg || echo "Not an Ubuntu distro. Exiting" 
 command -v curl || apt -y update ; apt -y install curl 
 
 curl -Lo /usr/local/bin/sift https://github.com/sans-dfir/sift-cli/releases/download/v1.7.1/sift-cli-linux
